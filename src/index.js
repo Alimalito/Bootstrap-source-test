71ed26b601ebff3bf943dd9e2d6076fb8e1e9b4c
Alimalito <8797800+Alimalito@users.noreply.github.com>
2022-07-18 21:02:45 +0100

    third commit

---
+++ src/index.js
@@ -0,0 +1,4 @@
+// Or import only needed plugins
+import * as bootstrap from 'bootstrap';
+// Rememeber to replace snippet when only need a few plugins
+// import { Tooltip as Tooltip, Toast as Toast, Popover as Popover } from 'bootstrap';
